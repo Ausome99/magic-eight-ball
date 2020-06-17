5d34eee7ddce7db12c36611fc31cb3a440580df7
austinmcdaniel <ausome99@gmail.com>
2020-06-16 18:09:36 -0600

    Initial commit

---
+++ magic_eight.py
@@ -0,0 +1,31 @@
+import random
+
+print("Ask the Magic 8 Ball a yes or no question")
+
+input("What is your question for the all knowing ball?\n")
+
+choices = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again."
+]
+
+choice = random.choice(choices)
+print(choice)
